d87e64ceb5df32f579613315839a7d6c8f048779
DUSI VENKATA SAI KARTHIKEYA <dvskartikeya@gmail.com>
2026-07-07 19:24:27 +0530

    Time: 0 ms (100%), Space: 19.4 MB (6%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,14 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        answers = [None] * ((len(nums)+1))
-        def maxMoney(n):
-            if n >= len(nums) :
-                return 0
-            if answers[n] is not None:
-                return answers[n]
-            answers[n] = max((nums[n] + maxMoney(n+2)), maxMoney(n+1))
-            return answers[n]
-        return maxMoney(0)
-            
-        
+        n = len(nums)
+        maxMoney = [0] * (n+2)
+        for i in range(n-1,-1,-1):
+            maxMoney[i] = max((nums[i] + maxMoney[i+2]), maxMoney[i+1])
+        return maxMoney[0]
         
