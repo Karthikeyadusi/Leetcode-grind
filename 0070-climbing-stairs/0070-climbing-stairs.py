eab5229e596a9bc609cc8379648a129f1afbefa7
DUSI VENKATA SAI KARTHIKEYA <dvskartikeya@gmail.com>
2026-07-07 15:17:20 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        answers = [None] * (n+1)
-        answers[0], answers[1] = 1,1
-        def solve(n):
-            if answers[n] is not None:
-                return answers[n]
-            else:
-                answers[n] = solve(n-1) + solve(n-2)
-                return answers[n]
-        return solve(n)
-        
+        prev1,prev2 = 1,1
+        for i in range(n-1):
+            current = prev1 + prev2
+            prev1 = prev2
+            prev2 = current
+        return prev2
         
