ac8eb50321d265ab3ac4dd937f89f2ea217c2895
DUSI VENKATA SAI KARTHIKEYA <dvskartikeya@gmail.com>
2026-07-08 14:59:23 +0530

    Time: 2006 ms (14.05%), Space: 20.1 MB (65.95%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,18 +1,18 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         memory = [None] * (len(nums) + 1)
-        def minJumps(n):
+        last = len(nums) - 1
+        memory[last] = 0
+        for i in range(last-1, -1, -1):
             answer = []
-            if n>= len(nums)-1:
-                return 0
-            if memory[n] is not None:
-                return memory[n]
-            if nums[n] == 0:
-                memory[n] = float("inf")
-                answer.append(memory[n])
-            for i in range(1, nums[n]+1):
-                answer.append(1 + minJumps(i+n))
-            memory[n] = min(answer)
-            return memory[n]
-        return minJumps(0)
+            if nums[i] == 0:
+                answer.append(float("inf"))
+            for j in range(1, nums[i]+1):
+                next = i + j
+                if next >= last:
+                    answer.append(1)
+                else:
+                    answer.append(1+memory[next])
+            memory[i] = min(answer)
+        return memory[0]
             
