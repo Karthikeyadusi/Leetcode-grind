be60881e639c259da39d7eabbfc5fe9b92d09494
DUSI VENKATA SAI KARTHIKEYA <dvskartikeya@gmail.com>
2026-08-07 10:57:30 +0530

    Time: 9 ms (81.09%), Space: 19.5 MB (30.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        hashmap1 = {}
+        hashmap2 = {}
+        for letter1 in s:
+            hashmap1[letter1] = hashmap1.get(letter1,0) + 1
+        for letter2 in t:
+            hashmap2[letter2] = hashmap2.get(letter2,0) + 1
+        return hashmap1 == hashmap2
+        
         
